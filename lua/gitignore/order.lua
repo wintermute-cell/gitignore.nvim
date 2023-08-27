36181bcd4ab769e11b8e3db7bebbe8c00c0b3588
Pavel Bialiauski <work.belyavsky.p.a@gmail.com>
2023-08-27 18:34:56 +0300

    fix: incorrect ordering
    In lua multiline string doesn't have access by keys. And every accessing
    is nil, so every result is zero. That is incorrect.
    
    There I fixed refresh_templates.sh script. Instead of pasting the same
    text I use looping by lines. Thus ignored commetaries and empty lines,
    aslo added number for every line that increase after pasting. The result
    is a table, which every key is template key and every value is ordered
    number from zero.
    
    With this, every key in lowercase. I can't find the script or command
    that can logically capitalize words by subwords. Therefore every
    template keys, which pass to the "order" table, must be in lowrcase.
    (lua/gitignore/init.lua)

--- lua/gitignore/order.lua
+++ lua/gitignore/order.lua
@@ -1,10 +1,8 @@
-local M = [[
-java
-gradle
-
-androidstudio
-
-visualstudio
-umbraco
-]]
+local M = {
+    java=0,
+    gradle=1,
+    androidstudio=2,
+    visualstudio=3,
+    umbraco=4,
+}
 return M
--- refresh_templates.sh
+++ refresh_templates.sh
@@ -7,9 +7,20 @@ git clone https://github.com/toptal/gitignore ./_repo
 TEMPLATE_STRING="local M = {}\n"
 for file in ./_repo/templates/*; do
     if [[ ${file#'./_repo/templates/'} == "order" ]]; then
-        printf "local M = [[\n$(cat $file | grep -v "^#")\n]]\nreturn M\n" > lua/gitignore/order.lua
+        order_lines="local M = {\n"
+        count=0
+        while read -r line
+        do
+            if [[  $line != "#"* && $line != "" ]];
+            then
+              order_lines="$order_lines    $line=$count,\n"
+              count=$((count + 1))
+            fi
+        done < "$file"
+        order_lines="$order_lines}\nreturn M\n"
+        printf "$order_lines" > lua/gitignore/order.lua
     else
-        TEMPLATE_STRING+="M[\"${file#'./_repo/templates/'}\"] = [[\n$(cat $file)\n]]\n"
+        TEMPLATE_STRING+="M[\"${file#'./_repo/templates/'}\"] = [[\n$(cat "$file")\n]]\n"
     fi
 done
 TEMPLATE_STRING="$TEMPLATE_STRING\nreturn M\n"
